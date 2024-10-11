cf4ffaf47ebd0ee2158e394ef55b8762605219d2
Andres Rivero <andres19rivero@gmail.com>
2024-10-11 16:09:47 -0400

    feat(entities): define user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  email: string;
+
+  @Prop()
+  firstName: string;
+
+  @Prop()
+  lastName: string;
+}
+
+export const UserModel = SchemaFactory.createForClass(User);
